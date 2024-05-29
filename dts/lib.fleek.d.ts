703aec4ceb7328a066ab21e322e89490d61c807e
ozwaldorf <self@ossian.dev>
2024-05-29 00:09:53 -0400

    chore: fleek namespace uses camelCase for functions now

--- dts/lib.fleek.d.ts
+++ dts/lib.fleek.d.ts
@@ -7,22 +7,22 @@ declare namespace Fleek {
    * @param {Uint8Array} hash - Blake3 hash of content to fetch
    * @returns {Promise<bool>} True if the fetch was successful
    */
-  function fetch_blake3(hash: Uint8Array): Promise<boolean>;
+  function fetchBlake3(hash: Uint8Array): Promise<boolean>;
   /** Load a blockstore handle to some blake3 content
    * @param {Uint8Array} hash - Blake3 hash of the content
    * @returns {Promise<ContentHandle>}
    */
-  function load_content(hash: Uint8Array): Promise<ContentHandle>;
+  function loadContent(hash: Uint8Array): Promise<ContentHandle>;
   /** Fetch a clients FLK balance.
    * @param {Uint8Array} account - The balance to check
    * @returns {Promise<BigInt>} BigInt of the balance
    */
-  function query_client_flk_balance(account: Uint8Array): Promise<BigInt>;
+  function queryClientFlkBalance(account: Uint8Array): Promise<BigInt>;
   /** Fetch a clients bandwidth balance.
    * @param {Uint8Array} account - The balance to check
    * @returns {Promise<BigInt>} BigInt of the balance
    */
-  function query_client_bandwidth_balance(
+  function queryClientBandwidthBalance(
     account: Uint8Array,
   ): Promise<BigInt>;
 
@@ -41,6 +41,6 @@ declare namespace Fleek {
       * @param {number} idx - Index of block to read
       * @returns {Promise<Uint8Array>}
       */
-    read(idx: number): Promise<Uint8Array>;
+    readBlock(idx: number): Promise<Uint8Array>;
   }
 }
